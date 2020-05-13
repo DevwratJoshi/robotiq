1eff5c6e19f8a102a1530268d7f068839984ea4a
Devwrat Joshi <devwratjoshi1234@gmail.com>
2020-05-13 10:13:11 +0900

    Remove project-specific names

--- robotiq_gazebo/scripts/gripper_action_controller_example.py
+++ robotiq_gazebo/scripts/gripper_action_controller_example.py
@@ -10,18 +10,30 @@ rospy.init_node('gripper_control')
      
     # Create an action client
     #Be sure to spawn the required controller beforehand, of type positionControllers/GripperActionController
-client = actionlib.SimpleActionClient('/a_bot_gripper_controller/gripper_cmd', GripperCommandAction)
+client = actionlib.SimpleActionClient('/gripper_controller/gripper_cmd', GripperCommandAction)
         
     # Wait until the action server has been started and is listening for goals
 client.wait_for_server()
 print("Found server")     
     # Create a goal to send (to the action server)
 goal = GripperCommandGoal()
-goal.command.position = 0.3   # From 0.0 to 0.8
-goal.command.max_effort = 10.0  # Do not limit the effort
+#Open the gripper
+goal.command.position = 0.0   #Completely open
+goal.command.max_effort = -1.0  # Do not limit the effort
 print(goal)
-print("Created goal message")
+print("Created gripper open goal message")
 client.send_goal(goal)
+client.wait_for_result()
+print(client.get_result())
 
+#Sleep for 1 second
+rospy.sleep(1)
+
+#Close the gripper
+goal.command.position = 0.85   #Completely closed
+goal.command.max_effort = -1.0  # Do not limit the effort
+print(goal)
+print("Created gripper open goal message")
+client.send_goal(goal)
 client.wait_for_result()
 print(client.get_result())
